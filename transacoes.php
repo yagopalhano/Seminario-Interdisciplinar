297797f489d278e32b0f034e57b12f2e17f23505
Yago Palhano <yago_1505@hotmail.com>
2022-07-11 18:13:22 -0300

    Update transacoes.php

--- transacoes.php
+++ transacoes.php
@@ -44,7 +44,7 @@ $transacoes = mysqli_fetch_all($db->query($query), 1);
             fetch("./scripts/excluitransacao.php", myInit)
                 .then(res => {
                     if (res.status === 200) {
-                        document.location = "https://seminario-interdisciplinar.herokuapp.com/transacoes.php"
+                        document.location = "https://seminario-interdisciplinar.herokuapp.com/"
                     }
                 })
                 .catch(rej => console.log(rej));

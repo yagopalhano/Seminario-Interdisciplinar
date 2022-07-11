c715f4f5f4646b435fec8b7201894cf92115f502
Yago Palhano <yago_1505@hotmail.com>
2022-07-11 14:49:27 -0300

    add

---
+++ scripts/dbConnection.php
@@ -0,0 +1,14 @@
+<?php
+    $dbhost = "lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbuser = "rtecz2xfd4vlc1fm";
+    $dbpass = "qy271l5uzj2a78qv";
+    $db = "vk14drj3tmp2hdan";
+    
+    // Create connection
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+    // Check connection
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully";
+?>

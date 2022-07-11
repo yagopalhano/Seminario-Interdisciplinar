dc27159d7b3ec85a624c13048bfaff868a41c990
Yago Palhano <yago_1505@hotmail.com>
2022-07-11 16:11:46 -0300

    bora

--- scripts/cadastro.php
+++ scripts/cadastro.php
@@ -1,6 +1,5 @@
 <?php
     $db = require("./dbConnection.php");
-    $tabelaUsuarios = require("../queries/tabelaUsuarios.sql")
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
@@ -9,14 +8,12 @@
     $qtdLetras = strlen($senhaCriptografada);
 
     $query = "Insert into Usuarios(Nome, Email, Senha) Values('$nome', '$email', '$senhaCriptografada');";
-    if($db->query($tabelaUsuarios);) {
-        if($db->query($query)) {
-            echo("http://$baseUrl/index.php?s=1");
-            header("Location: http://$baseUrl/index.php?s=1");
-            echo "Registro Realizado com sucesso";
-        }else {
-            echo "Error: " . $query . "<br>" . $conn->error;
-        }
+    
+    if($db->query($query)) {
+        header("Location: http://$baseUrl/index.php?s=1");
+        echo "Registro Realizado com sucesso";
+    }else {
+        echo "Error: " . $query . "<br>" . $conn->error;
     }
 
     $db->close();
--- scripts/dbConnection.php
+++ scripts/dbConnection.php
@@ -3,6 +3,10 @@
     $dbuser = "rtecz2xfd4vlc1fm";
     $dbpass = "qy271l5uzj2a78qv";
     $db = "vk14drj3tmp2hdan";
+
+    //Queries
+    $tabelaTipoTransacao = require("../queries/tabelaTipoTransacao.sql");
+    $tabelaUsuarios = require("../queries/tabelaUsuarios.sql");
     
     // Create connection
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
@@ -11,5 +15,11 @@
     die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
+    if (mysqli_query($conn, $tabelaUsuarios)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    } 
+    
     return $conn
 ?>

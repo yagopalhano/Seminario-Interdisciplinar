0900afc950233ea6dbe9357058ab2f8a9ba6b145
Yago Palhano <yago_1505@hotmail.com>
2022-07-11 15:07:44 -0300

    query ajust

--- scripts/cadastro.php
+++ scripts/cadastro.php
@@ -9,7 +9,7 @@
 
     $query = "Insert into Usuarios(Nome, Email, Senha) Values('$nome', '$email', '$senhaCriptografada');";
     
-    if($db->query($query)) {
+    if(mysqli_query($db, $query)) {
         header("Location: http://$baseUrl/index.php?s=1");
         echo "Registro Realizado com sucesso";
     }else {

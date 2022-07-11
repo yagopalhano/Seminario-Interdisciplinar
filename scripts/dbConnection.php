44608b5cc7a2aadd6975abb84821506147a71041
Yago Palhano <yago_1505@hotmail.com>
2022-07-11 17:32:17 -0300

    change

--- scripts/dbConnection.php
+++ scripts/dbConnection.php
@@ -14,12 +14,6 @@
     if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     }
-    echo "Connected successfully";
-    if (mysqli_query($conn, $tabelaUsuarios)) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    } 
     
     return $conn
 ?>
